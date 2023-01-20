3139f25cd8d235b9558a992fc2d4381b318a49fb
urmother <71238085+mei10101@users.noreply.github.com>
2023-01-20 12:10:33 -0800

    aya

---
+++ ayalumi/cache/wal/colors-wal-tabbed.h
@@ -0,0 +1,6 @@
+static const char* selbgcolor   = "#1a161d";
+static const char* selfgcolor   = "#e2e9f5";
+static const char* normbgcolor  = "#C8BACB";
+static const char* normfgcolor  = "#e2e9f5";
+static const char* urgbgcolor   = "#B3B2CA";
+static const char* urgfgcolor   = "#e2e9f5";
