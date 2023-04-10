5b0bc74a9313114660605ee5121460aedb1f6a03
Hyeonji <71238085+mei10101@users.noreply.github.com>
2023-04-10 00:49:42 -0700

    wqdwwqd

--- XORG/i3/yoru/cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c7c2c5", "#0a090c" },
-	[SchemeSel] = { "#c7c2c5", "#354B50" },
-	[SchemeOut] = { "#c7c2c5", "#8D7E81" },
-};
--- XORG/i3/yoru/cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#0a090c";
-static const char* selfgcolor   = "#c7c2c5";
-static const char* normbgcolor  = "#61595F";
-static const char* normfgcolor  = "#c7c2c5";
-static const char* urgbgcolor   = "#354B50";
-static const char* urgfgcolor   = "#c7c2c5";
